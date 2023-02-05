c9adaadc598a4949f0924a01bdc7ba7996d71612
Daglesia <daglesia.gaming@gmail.com>
2023-02-05 18:17:57 +0100

    deploy to github pages

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+set -e
+
+npm run build
+
+cd dist
+
+echo > .nojekyll
+
+
+git init
+git checkout -B main
+git add -A
+git commit -m 'deploy'
+
+git push -f git@github.com:Daglesia/Daglesia.github.io.git main
+
+cd -
